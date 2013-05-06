4ba2f7b0e84a3e3425c3ebac64409b6e68fc0898
Julien Deniau <julien.deniau@gmail.com>
2013-05-06 14:00:00 +0200

    update API

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 xbox-drone-controller
 =====================
 
-```
-npm install joystick
-npm install ar-drone
-```
+## Required librairies
+* joystick
+* ar-drone
+
+## Working buttons
+* start button to takeOff / land the drone
+* left stick to move the drone
+* right stick manages the altitude and the rotation
+* the cross is used to flip
+
+For the moment that's all, but feel free to help me improve it by opening an issue or a pull request !
+
+
--- xbox-controller.js
+++ xbox-controller.js
@@ -25,6 +25,9 @@ var axis = {
     3: 'right_x',
     4: 'right_y',
     5: 'rt',
+    6: 'cross_x',
+    7: 'cross_y'
+
 }
 
 var axisLimits = {
@@ -32,13 +35,15 @@ var axisLimits = {
     'max': 32767
 }
 
+var flipTime = 1500;
+
 start = function(event) {
     console.log('tlathoaeu')
 }
 
-button = function(event) {
+buttonAction = function(event) {
     if (event.init) {
-        console.log('init button ' + event.number);
+        console.log('init button ' + event.number + ' : ' + buttons[event.number]);
     } else {
         console.log(event);
         switch (buttons[event.number]) {
@@ -53,6 +58,7 @@ button = function(event) {
                     }
                     console.log(droneStatus);
                 }
+                break;
             case 'xbox':
                 // emergency !
                 break;
@@ -62,30 +68,82 @@ button = function(event) {
     }
 }
 
-axis = function(event) {
+axisAction = function(event) {
     if (event.init) {
-        console.log('init axis ' + event.number);
+        console.log('init axis ' + event.number + ' : ' + axis[event.number]);
     } else {
-        console.log(event);
-        switch (buttons[event.number]) {
+        switch (axis[event.number]) {
             case 'left_y':
                 if (event.value <= 0) {
-                    drone.front(event.value / axisLimits.min);
-                else {
-                    drone.back(event.value / axisLimits.max);
+                    s = event.value / axisLimits.min;
+                    drone.front(s);
+                } else {
+                    s = event.value / axisLimits.max;
+                    drone.back(s);
                 }
-           $case 'left_x':
+                console.log('front/back', s);
+                break;
+           case 'left_x':
                 if (event.value <= 0) {
-                    drone.left(event.value / axisLimits.min);
-                else {
-                    drone.right(event.value / axisLimits.max);
+                    s = event.value / axisLimits.min;
+                    drone.left(s);
+                } else {
+                    s = event.value / axisLimits.max;
+                    drone.right(s);
                 }
+                console.log('left/right', s);
+                break;
+            case 'right_y':
+                if (event.value <= 0) {
+                    s = event.value / axisLimits.min;
+                    drone.up(s);
+                } else {
+                    s = event.value / axisLimits.max;
+                    drone.down(s);
+                }
+                console.log('altitude', s);
+                break;
+           case 'right_x':
+                if (event.value <= 0) {
+                    s = event.value / axisLimits.min;
+                    drone.clockwise(s);
+                } else {
+                    s = event.value / axisLimits.max;
+                    drone.counterClockwise(s);
+                }
+                console.log('clockwise', s);
+                break;
+            case 'cross_x':
+                var action = null;
+                if (event.value > (axisLimits.max / 2)) {
+                    action = 'flipRight';
+                } else if (event.value < (axisLimits.min / 2)) {
+                    action = 'flipLeft';
+                }
+                if (action) {
+                    drone.animate(action, flipTime);
+                    console.log(action);
+                }
+                break;
+            case 'cross_y':
+                var action = null;
+                if (event.value > (axisLimits.max / 2)) {
+                    action = 'flipBehind';
+                } else if (event.value < (axisLimits.min / 2)) {
+                    action = 'flipAhead';
+                }
+                if (action) {
+                    drone.animate(action, flipTime);
+                    console.log(action);
+                }
+                break;
             default:
+                console.log(event);
                 break;
         }
     }
 }
 
-joystick.on('button', button);
-joystick.on('axis', axis);
+joystick.on('button', buttonAction);
+joystick.on('axis', axisAction);
 
